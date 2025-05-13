e7948f10cb4d6b6c5e0affb87794e5ea636ccb43
Wael Moharram <wael.moharram@gmail.com>
2025-05-13 03:39:38 +0300

    web message in main domain

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return 'Hello Ticket Goo';
+//    return ['Laravel' => app()->version()];
 });
 
 require __DIR__.'/auth.php';
